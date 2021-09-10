6c65947b19cce6802b9eab856f931d074194222a
NumahataSaori <saorin.kimura@gmail.com>
2021-09-10 13:11:17 +0000

    2021.09.10

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,54 @@
-{{-- layouts/create.blade.phpを読み込む --}}
 @extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
 
-
-{{-- layouts/profile.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィール作成画面')
-
-{{-- layouts/profile.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
+                <h2>プロフィール作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                        
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control-name" name="name" value="{{ old('name') }}" placeholder="名字 名前">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" class="form-control-gender" name="gender" value="{{ old('gender') }}">男
+                            <input type="radio" class="form-control-gender" name="gender" value="{{ old('gender') }}">女
+                            <input type="radio" class="form-control-gender" name="gender" value="{{ old('gender') }}">その他
+                        </div>
+                    </div>
+                    
+                     <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
+     
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create');
+     
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Route::get('XXX', 'AAAController@bbb');
-
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-});
 
 Auth::routes();
 
