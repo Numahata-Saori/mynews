53982c718197d4a1d19d0b68f9312a5d9f7e8c8d
NumahataSaori <saorin.kimura@gmail.com>
2021-09-14 14:50:31 +0000

    2021.09.14

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -5,9 +5,9 @@ return [
     'E-Mail Address' => 'メールアドレス',
     'Password' => 'パスワード',
     'Remember Me' => 'ログイン情報を記憶する',
-    'name' => '氏名'
-    'gender' => '性別'
-    'hobby' => '趣味'
+    'name' => '氏名',
+    'gender' => '性別',
+    'hobby' => '趣味',
     'introduction' => '自己紹介'
 ];
 
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,59 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="unf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Profile</title>
-    </head>
-    <body>
-        <h1>Profile編集画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('profile', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control-name" name="name" value="{{ $profile_form->name }}" placeholder="名字 名前">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control-gender" name="gender" value="{{ $profile_form->gender }}">
+                        </div>
+                    </div>
+                    
+                     <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ $profile_form->hobby }}</textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
