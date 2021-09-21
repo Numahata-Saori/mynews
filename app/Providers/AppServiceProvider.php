7b0656171bc90c5012f7cf10a85b3704c358afed
NumahataSaori <saorin.kimura@gmail.com>
2021-09-21 17:55:20 +0000

    new laravel project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        
-        if (\App::environment('production')) {
+        if (\App::environment(['staging', 'production'])) {
             \URL::forceScheme('https');
         }
-    
+        
     }
 }
