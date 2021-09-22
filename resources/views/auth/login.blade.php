9e30d59633d1aa061a0fa4f4ef79c44c41ec906c
NumahataSaori <saorin.kimura@gmail.com>
2021-09-22 13:20:04 +0000

    2021.09.22_aa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,15 +6,15 @@
             <div class="col-md-8">
                 <div class="login-box card">
                     
-                    //ヘルパ関数、「__(‘Login’)」→ この「__」は、ヘルパ関数（viewで使うための関数）の一種で、翻訳文字列の取得として使う
+                    {{--ヘルパ関数、「__(‘Login’)」→ この「__」は、ヘルパ関数（viewで使うための関数）の一種で、翻訳文字列の取得として使う--}}
                     <div class="login-header card-header mx-auto">{{ __('messages.Login') }}</div>
 
                     <div class="login-body card-body">
                         
-                        //route関数、{{ route('login') }} → URLを生成したりリダイレクトしたりするための関数、”/login”というURLを生成
+                        {{--route関数、{{ route('login') }} → URLを生成したりリダイレクトしたりするための関数、”/login”というURLを生成--}}
                         <form method="POST" action="{{ route('login') }}">
                             
-                            //認証済みのユーザーがリクエストを送信しているのかを確認するために利用
+                            {{--認証済みのユーザーがリクエストを送信しているのかを確認するために利用--}}
                             @csrf
 
                             <div class="form-group row">
@@ -22,18 +22,18 @@
 
                                 <div class="col-md-6">
                                     
-                                    //三項演算子、{{ $errors->has('email') ? ' is-invalid' : '' }}
-                                    //何もエラーがなければ$errors->has('email') の値はnullになるので:の右側が表示される
-                                    //エラーがある場合には$errors->has('email') の値にエラーメッセージが代入されるので、" is-invalid"が出力される
-                                    //$errorsというのは、Validationで返された時に代入されるメッセージが格納されている、そのままエラーメッセージが格納されている変数
-                                    //has(‘email’)は、emailフィールド（のこと）で発生したエラー内容という意味
-                                    //{{ old('email') }} → oldヘルパ関数は、セッションにフラッシュデータ（一時的にしか保存されないデータ）として入力されているデータを取得することができる
-                                    //フラッシュデータとは、直前までユーザーが入力していた値のこと
-                                    //直前のデータがない場合は、nullを返す
+                                    {{--三項演算子、{{ $errors->has('email') ? ' is-invalid' : '' }}
+                                    何もエラーがなければ$errors->has('email') の値はnullになるので:の右側が表示される
+                                    エラーがある場合には$errors->has('email') の値にエラーメッセージが代入されるので、" is-invalid"が出力される
+                                    $errorsというのは、Validationで返された時に代入されるメッセージが格納されている、そのままエラーメッセージが格納されている変数
+                                    has(‘email’)は、emailフィールド（のこと）で発生したエラー内容という意味
+                                    {{ old('email') }} → oldヘルパ関数は、セッションにフラッシュデータ（一時的にしか保存されないデータ）として入力されているデータを取得することができる
+                                    フラッシュデータとは、直前までユーザーが入力していた値のこと
+                                    直前のデータがない場合は、nullを返す--}}
                                     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                                    //@ifと@endifは、phpのif文を意味
-                                    //($errors->has('email')) → エラーがあればエラーメッセージを、なければnullを返す
+                                    {{--@ifと@endifは、phpのif文を意味
+                                    ($errors->has('email')) → エラーがあればエラーメッセージを、なければnullを返す--}}
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback">
                                             <strong>{{ $errors->first('email') }}</strong>
